4a949f88419afa73dfb147a2c6b76bd71fcca064
damien-vaneberck <damien.vaneberck@student.uclouvain.be>
2016-03-12 16:06:21 +0100

    main with test for calloc

--- src/Main.c
+++ src/Main.c
@@ -4,9 +4,16 @@
 
 int main(int argc, const char *argv[])
 {
-	myalloc(64);
-	int *test = (int *)myalloc(sizeof(int));
+	mymalloc(64);
+	int *test = (int *)mymalloc(sizeof(int));
 	*test = 42;
 	printf("la valeur est : %d \n", *test);
+	myfree(test);
+	int *test2 = (int *)mymalloc(sizeof(int));
+	printf("la valeur est : %d \n", *test2);
+	myfree(test2);
+	int *test3 = (int *)mycalloc(sizeof(int));
+	printf("la valeur est : %d \n", *test3);
+	
 	return EXIT_SUCCESS;
 } 
